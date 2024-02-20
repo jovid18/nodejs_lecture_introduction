1db5541377eac6c1901733c3ffb9afdfff8c1be4
jovid_18 <cshcho99@gmail.com>
2024-02-20 17:24:57 +0900

    2.5

--- week2/spa-shop/routes/goods.js
+++ week2/spa-shop/routes/goods.js
@@ -1,73 +1,25 @@
 import express from 'express';
 
 const router = express.Router();
+import Goods from '../schemas/goods.js';
 
-//routes/goods.js
-//상품 예시 데이터
-const goods = [
-  {
-    goodsId: 1,
-    name: '상품 1',
-    thumbnailUrl:
-      'https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg',
-    category: 'drink',
-    price: 6.2,
-  },
-  {
-    goodsId: 2,
-    name: '상품 2',
-    thumbnailUrl:
-      'https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg',
-    category: 'drink',
-    price: 0.11,
-  },
-  {
-    goodsId: 3,
-    name: '상품 3',
-    thumbnailUrl:
-      'https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg',
-    category: 'drink',
-    price: 2.2,
-  },
-  {
-    goodsId: 4,
-    name: '상품 4',
-    thumbnailUrl:
-      'https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg',
-    category: 'drink',
-    price: 0.1,
-  },
-];
-//상품 목록 조회 API
-router.get('/goods', (req, res) => {
-  return res.status(200).json({
-    goods: goods,
-  });
-});
-
-router.get('/goods/:goodsId', (req, res) => {
-  const goodsId = req.params.goodsId;
-  const findGoods = goods.find((item) => item.goodsId == goodsId);
-  return res.status(200).json({
-    goods: findGoods,
-  });
-});
-
-//상품 등록 API
-router.post('/goods', (req, res) => {
-  const { name, thumbnailUrl, category, price } = req.body;
-  const goodsId = goods[goods.length - 1].goodsId + 1;
-  const newGoods = {
+import mongoose from 'mongoose';
+router.post('/goods', async (req, res) => {
+  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+  const goods = await Goods.find({ goodsId: goodsId }).exec();
+  if (goods.length) {
+    return res
+      .status(400)
+      .json({ errorMessage: '이미 존재하는 데이터입니다.' });
+  }
+  const createGoods = await Goods.create({
     goodsId,
     name,
     thumbnailUrl,
     category,
     price,
-  };
-  goods.push(newGoods);
-  return res.status(200).json({
-    goods: newGoods,
   });
+  return res.status(201).json({ goods: createGoods });
 });
 
 export default router;
