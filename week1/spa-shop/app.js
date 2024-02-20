61322c09faceaa21ba666ea93014f3e782c8f4c4
jovid_18 <cshcho99@gmail.com>
2024-02-20 15:01:20 +0900

    1.19

--- week1/spa-shop/app.js
+++ week1/spa-shop/app.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import goodsRouter from './routes/goods.js';
+import newsRouter from './routes/news.js';
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api', [goodsRouter]);
+app.use('/api', [goodsRouter, newsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
